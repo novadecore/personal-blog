29f77995a24bbd4039c9eefff88aa88cedf4af16
novmdecore <2015200799@alu.ruc.edu>
2025-12-03 21:17:15 -0800

    fix bug

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -48,11 +48,13 @@ router.post('/login', async (req, res) => {
       { expiresIn: '12h' }
     );
 
+    const isProd = process.env.NODE_ENV === 'production';
+
     res.cookie('token', token, {
       httpOnly: true,
       sameSite: isProd ? 'none' : 'lax',
       secure: isProd,
-      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+      maxAge: 7 * 24 * 60 * 60 * 1000,     // 7 days
     });
 
     res.json({
